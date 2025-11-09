0ccd8ee75c8ffbc04ead9d82836e0d216c92c85c
Astral1w3 <angelomarcone4@gmail.com>
2025-11-09 12:44:26 +0100

    changed server location

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
-    //baseURL: 'https://dietiestates2025backendv2-hfehdnhuabfefmg7.italynorth-01.azurewebsites.net/api'
+    //baseURL: 'http://localhost:8080/api'
+    baseURL: 'https://dietiestates2025backendv2-hfehdnhuabfefmg7.italynorth-01.azurewebsites.net/api'
     //baseURL: 'https://dietiestates2025backend-d6fwbjhfhgebe2c7.canadacentral-01.azurewebsites.net/api'
 });
 
